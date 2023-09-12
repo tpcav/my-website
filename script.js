5a131496fb78a9c54d00cc39b7ff2779595e304a
c <thomascavalcante77@gmail.com>
2023-09-11 22:04:12 -0400

    testing 28

--- script.js
+++
@@ -1,9 +0,0 @@
-        // Read the contents of name.txt and set it as the content of the 'namePlaceholder' span
-        fetch('name.txt')
-            .then(response => response.text())
-            .then(name => {
-                document.getElementById('namePlaceholder').textContent = name;
-            })
-            .catch(error => {
-                console.error('Error reading name.txt:', error);
-            });
