52fd1d4c92528b13288ba0a8ba7f5bbabaa7515d
c <thomascavalcante77@gmail.com>
2023-09-11 21:38:35 -0400

    testing 21

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
-// Function to fetch and parse YAML from a URL
-async function fetchAndParseYAML(url) {
-  try {
-    const response = await fetch(url);
+document.addEventListener("DOMContentLoaded", function () {
+  // Access the 'name' input value from the URL query parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  const name = urlParams.get("name");
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+  // Get the HTML element where you want to display the name
+  const nameDisplayElement = document.getElementById("nameDisplay");
 
-    const yamlText = await response.text();
-    const yamlData = jsyaml.load(yamlText);
-
-    if (!yamlData || !yamlData.inputs || !yamlData.inputs.name) {  // Update to 'name'
-      throw new Error('Name not found in YAML data');  // Update the error message
-    }
-
-    return yamlData.inputs.name;  // Update to 'name'
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
+  if (nameDisplayElement) {
+      // Update the content of the element with the name value
+      nameDisplayElement.textContent = name;
   }
-}
-
-// Element where you want to display the YAML content
-const yamlCodeElement = document.getElementById('namePlaceholder');
-
-// URL of the YAML file on GitHub
-const yamlFileUrl =
-  'https://raw.githubusercontent.com/tpcav/my-website/main/.github/workflows/hello.world.yaml';
-
-// Fetch and display the YAML code
-fetchAndParseYAML(yamlFileUrl)
-  .then((name) => {  // Update to 'name'
-    displayCodeInElement(name, yamlCodeElement);  // Update to 'name'
-  })
-  .catch((error) => {
-    yamlCodeElement.textContent = `Error: ${error.message}`;
-  });
+});
