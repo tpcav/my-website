12fec78b67cd697bac0b9c700834559cd0fa4864
c <thomascavalcante77@gmail.com>
2023-09-11 21:54:02 -0400

    testing 25

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch the YAML content from an external source (e.g., GitHub raw content)
-  fetch('https://raw.githubusercontent.com/tpcav/my-website/main/.github/workflows/hello.world.yaml')
-      .then(response => response.text())
-      .then(yamlText => {
-          // Parse the YAML content using js-yaml
-          const yamlData = jsyaml.load(yamlText);
-
-          // Access and display specific data from the YAML, e.g., inputs.name
-          const name = yamlData.on.workflow_dispatch.inputs.name;
-
-          // Get the HTML element where you want to display the name
-          const nameDisplayElement = document.getElementById("nameDisplay");
-
-          if (nameDisplayElement) {
-              // Update the content of the element with the name value
-              nameDisplayElement.textContent = name;
-          }
-      })
-      .catch(error => {
-          console.error('Error fetching or parsing YAML:', error);
-      });
-});
+        // Read the contents of name.txt and set it as the content of the 'namePlaceholder' span
+        fetch('name.txt')
+            .then(response => response.text())
+            .then(name => {
+                document.getElementById('namePlaceholder').textContent = name;
+            })
+            .catch(error => {
+                console.error('Error reading name.txt:', error);
+            });
