468f25e500d95c0d512581563e3978b89ec38114
c <thomascavalcante77@gmail.com>
2023-09-11 20:06:45 -0400

    yaml file stuff fixed

--- script.js
+++ script.js
@@ -4,21 +4,32 @@
  // Element where you want to display the HTML code
  const yamlCodeElement = document.getElementById('yamlCode');
 
- // Fetch the YAML file
- fetch(yamlFileUrl)
-     .then((response) => response.text())
-     .then((yamlText) => {
-         // Parse the YAML data
-         const yamlData = jsyaml.load(yamlText);
+// Fetch the YAML file
+fetch(yamlFileUrl)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then((yamlText) => {
+        try {
+            // Parse the YAML data
+            const yamlData = jsyaml.load(yamlText);
 
-         // Check if the 'code' key exists in the YAML data
-         if (yamlData.hasOwnProperty('code')) {
-             // Display the 'code' property value in the HTML element as text
-             yamlCodeElement.textContent = yamlData.code;
-         } else {
-             yamlCodeElement.textContent = 'Code not found in YAML data';
-         }
-     })
-     .catch((error) => {
-         console.error('Error fetching or parsing YAML:', error);
-     });
+            // Check if the 'code' key exists in the YAML data
+            if (yamlData && yamlData.hasOwnProperty('name')) {
+                // Set the innerHTML of the yamlCodeElement to the 'code' property value
+                yamlCodeElement.innerHTML = yamlData.name;
+            } else {
+                yamlCodeElement.textContent = 'Code not found in YAML data';
+            }
+        } catch (error) {
+            console.error('Error parsing YAML:', error);
+            yamlCodeElement.textContent = 'Error parsing YAML data';
+        }
+    })
+    .catch((error) => {
+        console.error('Error fetching YAML:', error);
+        yamlCodeElement.textContent = 'Error fetching YAML data';
+    });
