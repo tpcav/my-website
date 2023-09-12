34a826245b96e9f048c4179e22bf1ef897fc9313
c <thomascavalcante77@gmail.com>
2023-09-11 21:30:02 -0400

    testing 19

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
-// URL of the YAML file on GitHub
-const yamlFileUrl = 'https://raw.githubusercontent.com/tpcav/my-website/main/.github/workflows/hello.world.yaml';
+// Function to fetch and parse YAML from a URL
+async function fetchAndParseYAML(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const yamlText = await response.text();
+    const yamlData = jsyaml.load(yamlText);
+
+    if (!yamlData || !yamlData.inputs || !yamlData.inputs.code) {
+      throw new Error('Code not found in YAML data');
+    }
+
+    return yamlData.inputs.code;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+// Function to display code in an element
+function displayCodeInElement(code, element) {
+  const h1Element = document.createElement('h1');
+  h1Element.textContent = code;
+  element.appendChild(h1Element);
+}
 
 // Element where you want to display the YAML content
 const yamlCodeElement = document.getElementById('yamlCode');
 
-// Fetch the YAML file
-fetch(yamlFileUrl)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.text();
-    })
-    .then((yamlText) => {
-        try {
-            // Parse the YAML data
-            const yamlData = jsyaml.load(yamlText);
+// URL of the YAML file on GitHub
+const yamlFileUrl =
+  'https://raw.githubusercontent.com/tpcav/my-website/main/.github/workflows/hello.world.yaml';
 
-            // Check if the 'inputs' object exists in the YAML data and has a 'code' property
-            if (yamlData && yamlData.inputs && yamlData.inputs.code) {
-                // Create an <h1> element and set its text content to the 'code' property value
-                const h1Element = document.createElement('h1');
-                h1Element.textContent = yamlData.inputs.code;
-
-                // Append the <h1> element to the yamlCodeElement
-                yamlCodeElement.appendChild(h1Element);
-            } else {
-                // Display an error message if 'inputs.code' is not found in YAML data
-                yamlCodeElement.textContent = 'Code not found in YAML data';
-            }
-        } catch (error) {
-            console.error('Error parsing YAML:', error);
-            yamlCodeElement.textContent = 'Error parsing YAML data';
-        }
-    })
-    .catch((error) => {
-        console.error('Error fetching YAML:', error);
-        yamlCodeElement.textContent = 'Error fetching YAML data';
-    });
+// Fetch and display the YAML code
+fetchAndParseYAML(yamlFileUrl)
+  .then((code) => {
+    displayCodeInElement(code, yamlCodeElement);
+  })
+  .catch((error) => {
+    yamlCodeElement.textContent = `Error: ${error.message}`;
+  });
